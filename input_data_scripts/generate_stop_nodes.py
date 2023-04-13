a72feea40b95492f5723849e0a5705c9c957ea67
Alex Eckardt <realalexeck@gmail.com>
2023-04-13 19:24:47 -0400

    New Graph Class

--- input_data_scripts/generate_stop_nodes.py
+++ input_data_scripts/generate_stop_nodes.py
@@ -1,31 +1,61 @@
 
 class Node:
-    def __init__(self, name):
+    def __init__(self, stop_id, name):
+        self.id = stop_id
         self.name = name
 
     def __repr__(self):
         return self.name
 
+class Edge:
+    def __init__(self, name):
+        self.name = name
 
-with open('input_data_scripts/in_GTFS/stops.txt', 'r') as f:
+    def __repr__(self):
+        return self.name
+    
+class Graph:
+    def __init__(self):
+        self.adj = {}
 
-    stops = {}
+    #
+    def add_node(self, node):
 
-    # Drop Heading Line
-    line = f.readline();
-    line = f.readline().strip();
+        #Make Sure Not Overriding
+        if (self.adj.get(node.id, '{}') != {}):
+            self.adj[node.id] = {}
+        else:
+            raise Exception(f"Node {node.id} exists in graph already!")
+        
+    #
+    def add_edge(self):
+        pass
 
-    #Loop
-    while line != '':
+    def __str__(self):
+        return str(self.adj)
+    
+if __name__ == '__main__':
+        
+    with open('input_data_scripts/in_GTFS/stops.txt', 'r') as f:
 
-        stop_id,stop_name,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station,wheelchair_boarding,stop_code = line.split(',')
-        stopNode = Node(stop_name)
+        G = Graph()
 
-        stops[stop_id] = stopNode;
-
-        #Continue
+        # Drop Heading Line
+        line = f.readline();
         line = f.readline().strip();
 
-    # Done, Generated Nodes
+        #Loop
+        while line != '':
 
-    print(stops)
+            stop_id,stop_name,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station,wheelchair_boarding,stop_code = line.split(',')
+            stopNode = Node(stop_id, stop_name)
+
+            G.add_node(stopNode)
+
+            #Continue
+            line = f.readline().strip();
+
+        # Done, Generated Nodes
+
+        # Print Adj List
+        print(G)
