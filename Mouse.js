f66047f5b1b1a920434752b24d04cf677a7a9ed2
Alex Eckardt <realalexeck@gmail.com>
2023-04-17 19:12:17 -0400

    Mouse File

---
+++ Mouse.js
@@ -0,0 +1,88 @@
+import { Vector2 } from "./helper.js";
+import { cam, goalCamW, goalCamH } from "./Camera.js";
+import {gui_coords_to_real_coords } from "./Coordinates.js";
+
+function Mouse() {
+    this.gui_position = new Vector2(0, 0);
+}
+
+export let mouse = new Mouse();
+let dragStartMousePos = new Vector2(0, 0);
+let dragStartCamPos = new Vector2(0, 0);
+let dragging = false;
+
+export function setupMouse() {
+
+    (function() {
+        document.onmousemove = handleMouseMove;
+        function handleMouseMove(event) {
+            var eventDoc, doc, body;
+
+            event = event || window.event; // IE-ism
+
+            // If pageX/Y aren't available and clientX/Y are,
+            // calculate pageX/Y - logic taken from jQuery.
+            // (This is to support old IE)
+            if (event.pageX == null && event.clientX != null) {
+                eventDoc = (event.target && event.target.ownerDocument) || document;
+                doc = eventDoc.documentElement;
+                body = eventDoc.body;
+
+                event.pageX = event.clientX +
+                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                (doc && doc.clientLeft || body && body.clientLeft || 0);
+                event.pageY = event.clientY +
+                (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+                (doc && doc.clientTop  || body && body.clientTop  || 0 );
+            }
+
+            mouse.gui_position = new Vector2(event.pageX, event.pageY); 
+           // console.log(gui_coords_to_real_coords(mouse.gui_position));
+
+            // Use event.pageX / event.pageY here
+            if (dragging) {
+
+                //Add to Difference
+                let x = dragStartCamPos.x + (mouse.gui_position.x - dragStartMousePos.x);
+                let y = dragStartCamPos.y + (mouse.gui_position.y - dragStartMousePos.y);
+
+                cam.position = new Vector2(x, y);
+                //console.log(cam)
+            }
+
+            //Update
+            mouse.latlon_position = new Vector2();
+        }
+    })();
+
+    //Mouse Events
+    window.addEventListener('mousedown', (event) => {
+        console.log("Mouse Down");
+        dragging = true;
+        dragStartMousePos = mouse.gui_position;
+        dragStartCamPos = cam.position;
+    });
+
+    window.addEventListener('mouseup', (event) => {
+        console.log("Mouse Up");
+        dragging = false;
+    });
+
+    window.addEventListener('wheel', (event) => {
+        
+        //Get
+        let deltaY = event.deltaY > 0
+
+        //Scroll Up
+        if (event.deltaY > 0) {
+
+            cam.zoom_in();
+            return;
+            
+        }
+        
+        //Scroll Down
+        cam.zoom_out();
+    });
+
+}
